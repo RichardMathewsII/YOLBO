13b82704b9cfa683cb13ae5406c6878f586df5e1
RichardMathewsII <52604283+RichardMathewsII@users.noreply.github.com>
2020-05-15 14:13:08 -0500

    Fix bug

--- visualization.py
+++ visualization.py
@@ -12,11 +12,15 @@ def draw_label(frame, label, box, color):
     cv2.putText(frame, label, (b[0] + 3, b[1] - baseline), font, 1, (0, 0, 0), 1)
 
 
-def annotate_frame(frame, frame_number, frame_height, num_boxes):
+def annotate_frame(frame, frame_number, frame_height, num_boxes, yolbo=False):
 
     cv2.putText(frame, 'Frame: ' + str(frame_number), (20, 40), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 5)
     cv2.putText(frame, 'Frame: ' + str(frame_number), (20, 40), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 2)
     cv2.putText(frame, str(num_boxes) + ' Boxes', (20, 90), cv2.FONT_HERSHEY_PLAIN, 3, (0, 0, 0), 5)
     cv2.putText(frame, str(num_boxes) + ' Boxes', (20, 90), cv2.FONT_HERSHEY_PLAIN, 3, (255, 255, 255), 2)
-    cv2.putText(frame, 'RetinaNet', (20, frame_height - 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 4)
-    cv2.putText(frame, 'RetinaNet', (20, frame_height - 20), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+    if yolbo:
+        cv2.putText(frame, 'Y.O.L.B.O.', (20, frame_height - 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 4)
+        cv2.putText(frame, 'Y.O.L.B.O.', (20, frame_height - 20), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
+    else:
+        cv2.putText(frame, 'RetinaNet', (20, frame_height - 20), cv2.FONT_HERSHEY_PLAIN, 2, (0, 0, 0), 4)
+        cv2.putText(frame, 'RetinaNet', (20, frame_height - 20), cv2.FONT_HERSHEY_PLAIN, 2, (255, 255, 255), 2)
